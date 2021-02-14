de41376fc160cada885fa75241d1ee4cfed929fe
Vladimirsky <vladimir.bervin@gmail.com>
2021-02-14 17:04:59 +0300

    Реализация отображения категорий и всего списка задач в реактивном стиле

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,26 +2,29 @@ import { Injectable } from '@angular/core';
 import {Category} from '../Model/Category';
 import {Task} from '../Model/Task';
 import {TestData} from '../data/TestData';
-import {Subject} from 'rxjs';
+import {BehaviorSubject, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  tasksSubject = new Subject<Task[]>();
+  // @ts-ignore
+  tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories);
 
   constructor() {
   }
 
-  getCategories(): Category[]{
-    return TestData.categories;
-  }
+  // getCategories(): Category[]{
+  //   return TestData.categories;
+  // }
 
   // getTasks(): Task[]{
   //     return TestData.tasks;
   //   }
 
+  // tslint:disable-next-line:typedef
   fillTasks() {
     this.tasksSubject.next(TestData.tasks);
   }
@@ -31,6 +34,7 @@ export class DataHandlerService {
   //   return tasks;
   // }
 
+  // tslint:disable-next-line:typedef
   fillTasksByCategory(category: Category){
     const tasks = TestData.tasks.filter(task => task.category === category);
     this.tasksSubject.next(tasks);
--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -16,7 +16,8 @@ export class CategoriesComponent implements OnInit {
 
   // Метод вызывыается автоматически после инициализации компонента
   ngOnInit(): void {
-    this.categories = this.dataHandler.getCategories();
+    // this.categories = this.dataHandler.getCategories();
+    this.dataHandler.categoriesSubject.subscribe(categories => this.categories = categories);
   }
 
   // tslint:disable-next-line:typedef
